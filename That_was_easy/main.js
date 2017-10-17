bbe989879be1577b0dd7c074b3817f56c3de72c2
MoroniMB <moroni.montoyabernal@masonohioschools.com>
2017-10-17 19:07:57 +0000

    create that was easy button

--- That_was_easy/main.js
+++ That_was_easy/main.js
@@ -1,10 +1,13 @@
-var thatWasEasy;
-var thatWasEasy = new Audio("https://raw.githubusercontent.com/hackclub/hackclub/master/workshops/that_was_easy/sounds/that_was_easy.mp3");
-thatWasEasy.play(); 
-
 function saythatwaseasy(){
     var thatWasEasy = new Audio("https://raw.githubusercontent.com/hackclub/hackclub/master/workshops/that_was_easy/sounds/that_was_easy.mp3");
     thatWasEasy.play();
 }
-$("easy").on("click")
-$("#easy").on("click", saythatwaseasy);
+/*$*/("#easy").on("click", saythatwaseasy);
+
+/*$*/(document).keypress(delegatekeypress)
+
+function delegatekeypress(event) {
+    if (event.keycode ==32) {
+        /*$*/("#easy").trigger("click");
+    }
+}
